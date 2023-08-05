b8b47803c43ca13542dcfdacca2ed135470eebc9
WZNT-KimHyeJin <pcrs5278@naver.com>
2023-08-05 14:28:31 +0900

    init

---
+++ src/test/java/likelion_hkt/ll_hkt_be/LlHktBeApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion_hkt.ll_hkt_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlHktBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
