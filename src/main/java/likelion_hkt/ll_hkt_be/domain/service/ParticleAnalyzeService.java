95126211b269073ec3ab7226d122847a07be3fae
daemin-kim <kdm0068@gmail.com>
2023-08-09 18:11:23 +0900

    [fix] 문장 안에서 단어를 찾아도 엑셀에서 정보를 제대로 가져오지 못하던 오류 수정, 공백을 가진 표현 검색 가능하게 수정

--- src/main/java/likelion_hkt/ll_hkt_be/domain/service/ParticleAnalyzeService.java
+++ src/main/java/likelion_hkt/ll_hkt_be/domain/service/ParticleAnalyzeService.java
@@ -11,45 +11,31 @@ import java.util.regex.Pattern;
 public class ParticleAnalyzeService {
     private static final Pattern PARTICLE_PATTERN = Pattern.compile("웃프(다|네|)"); // 조사
     private static final Map<Pattern, String> patternTranslations = new HashMap<>();
-    private static final Map<String, String> spacedWordsTranslations = new HashMap<>();
-
 
     static {
         // 조사 패턴
-        patternTranslations.put(PARTICLE_PATTERN, "웃긴데 사실 슬프$1");
+        patternTranslations.put(PARTICLE_PATTERN, "웃프다");
 
         // 접두사, 접미사 패턴
-        patternTranslations.put(Pattern.compile("\\b킹(\\w*?)"), "완전 $1");
-        patternTranslations.put(Pattern.compile("\\b(.)밍아웃\\b"), "취향 공개");
-        patternTranslations.put(Pattern.compile("(\\w*?)빠(\\w*?)"), "$1팬이$2");
-        patternTranslations.put(Pattern.compile("\\b짱(\\w*?)"), "최고$1");
-        patternTranslations.put(Pattern.compile("(\\w*?)충(\\w*?)"), "$1 하는 사람$2");
-        patternTranslations.put(Pattern.compile("\\b캐(\\w*?)"), "엄청 $1");
-        patternTranslations.put(Pattern.compile("\\b핵(\\w*?)"), "진짜 $1");
-        patternTranslations.put(Pattern.compile("(\\w*?)까(\\w*?)"), "$1 안티팬$2");
-        patternTranslations.put(Pattern.compile("\\b대깨(\\w*?)"), "무조건 $1");
-        patternTranslations.put(Pattern.compile("(\\w*?)각[^!?]*"), "$1할 수 있겠다");
-
-        // 원래 띄어쓰기가 있는 단어
-        spacedWordsTranslations.put("라떼는 말이야", "나 때는 말이야");
-        spacedWordsTranslations.put("무슨 129", "무슨 일이야");
-        spacedWordsTranslations.put("아무 말 대잔치", "아무말하기");
-        spacedWordsTranslations.put("의문의 1승", "의도하지 않은 이득");
-        spacedWordsTranslations.put("혈중 마라 농도", "마라 음식 섭취 빈도");
-        spacedWordsTranslations.put("서로서로 도와요", "다 같이 해요");
-        spacedWordsTranslations.put("누가 기침 소리를 내었어", "누가 그랬어");
+        patternTranslations.put(Pattern.compile("킹(\\w*?)"), "킹 $1");
+        patternTranslations.put(Pattern.compile("짱(\\w*?)"), "짱 $1");
+        patternTranslations.put(Pattern.compile("캐(\\w*?)"), "캐- $1");
+        patternTranslations.put(Pattern.compile("핵(\\w*?)"), "핵- $1");
+        patternTranslations.put(Pattern.compile("대깨(\\w*?)"),"대깨- $1");
+        patternTranslations.put(Pattern.compile("(.)?밍아웃"), "$1 -밍아웃");
+        patternTranslations.put(Pattern.compile("(\\w*?)빠(\\w*?)"), "$1 빠 $2");
+        patternTranslations.put(Pattern.compile("(\\w*?)충(\\w*?)"), "$1 -충 $2");
+        patternTranslations.put(Pattern.compile("(\\w*?)까(\\w*?)"), "$1 -까 $2");
+        patternTranslations.put(Pattern.compile("(\\w*?)각(\\w*?)"), "$1 -각 $2");
     }
 
     public String InitialSentenceAnalysis(String text) {
-        // 띄어쓰기 있는 단어 변환
-        for (Map.Entry<String, String> entry : spacedWordsTranslations.entrySet()) {
-            text = text.replace(entry.getKey(), entry.getValue());
-        }
-
         // 조사 및 접두사, 접미사 변환
         for (Map.Entry<Pattern, String> entry : patternTranslations.entrySet()) {
             Matcher matcher = entry.getKey().matcher(text);
-            text = matcher.replaceAll(entry.getValue());
+            if (matcher.find()) {
+                text = matcher.replaceAll(entry.getValue());
+            }
         }
         return text;
     }
